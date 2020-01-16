15e76e6e176f9673230a4a958db2d6219204bee6
eliasasgard <elias@sabyposten.se>
2020-01-16 12:24:06 +0100

    Add files via upload

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,20 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    canBeShared: true
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    canBeShared: true
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    canBeShared: false
   }
 ];
 
